f4801afb5ed557bc48e532a99c6f7d248545f4b1
Student <student@itstep.local>
2022-06-01 20:10:20 +0300

    Настройка отображения моделей в админке

--- demo/application/admin.py
+++ demo/application/admin.py
@@ -1,18 +1,35 @@
 from django.contrib import admin
-# from django.contrib.admin import AdminSite
 from .models import (
     Question,
     Choice
 )
 
 
-# class ApplicationAdminSite(AdminSite):
-#     site_header = 'Тестовое приложение опросов'
-#
-#
-# app_admin = ApplicationAdminSite(name="application_admin")
-# app_admin.register(Question)
-# app_admin.register(Choice)
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 3
 
-admin.site.register(Question)
-admin.site.register(Choice)
+
+class QuestionAdmin(admin.ModelAdmin):
+    # fields = ['pub_date', 'question_text']
+    fieldsets = [
+        (
+            None,
+            {
+                'fields': ['question_text']
+            }
+        ),
+        (
+            'Date information',
+            {
+                'fields': ['pub_date']
+            }
+        ),
+    ]
+    inlines = [
+        ChoiceInline
+    ]
+
+
+admin.site.register(Question, QuestionAdmin)
+# admin.site.register(Choice)
